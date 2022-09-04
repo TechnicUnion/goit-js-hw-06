27a4517efc26a44120450d11f66c0b21a90649b7
TechnicUnion <103375230+TechnicUnion@users.noreply.github.com>
2022-09-04 22:48:36 +0300

    change task 2

--- js/task-02.js
+++ js/task-02.js
@@ -9,11 +9,21 @@ const ingredients = [
 
 const listEL = document.querySelector("#ingredients");
 
-const ingredientsListEl = ingredients.map((ingredient) => {
-  const ingredientsItemEl = document.createElement("li");
-  ingredientsItemEl.classList.add("item");
-  ingredientsItemEl.textContent = ingredient;
-  return ingredientsItemEl;
-});
+// const ingredientsListEl = ingredients.map((ingredient) => {
+//   const ingredientsItemEl = document.createElement("li");
+//   ingredientsItemEl.classList.add("item");
+//   ingredientsItemEl.textContent = ingredient;
+//   return ingredientsItemEl;
+// });
 
+const makeIngredientsList = (item) => {
+  return item.map((ingredient) => {
+    const ingredientsItemEl = document.createElement("li");
+    ingredientsItemEl.classList.add("item");
+    ingredientsItemEl.textContent = ingredient;
+    return ingredientsItemEl;
+  });
+};
+
+const ingredientsListEl = makeIngredientsList(ingredients);
 listEL.append(...ingredientsListEl);
