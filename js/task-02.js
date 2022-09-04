4ec3afd387ce05b95240a26213acf05f230e758a
TechnicUnion <103375230+TechnicUnion@users.noreply.github.com>
2022-09-04 20:15:19 +0300

    adds task 2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,19 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const listEL = document.querySelector("#ingredients");
+
+const ingredientsListEl = ingredients.map((ingredient) => {
+  const ingredientsItemEl = document.createElement("li");
+  ingredientsItemEl.classList.add("item");
+  ingredientsItemEl.textContent = ingredient;
+  return ingredientsItemEl;
+});
+
+listEL.append(...ingredientsListEl);
