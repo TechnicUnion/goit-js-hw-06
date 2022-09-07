00d077e6d024acc14fbc0ee21c9c357109d82419
TechnicUnion <103375230+TechnicUnion@users.noreply.github.com>
2022-09-07 22:50:00 +0300

    adds task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,23 @@
+const form = document.querySelector(".login-form");
+const btnEl = document.querySelector("button");
 
+form.addEventListener("submit", onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  const formElements = event.currentTarget.elements;
+  const email = formElements.email.value;
+  const password = formElements.password.value;
+
+  if (email && password) {
+    const formData = {
+      email,
+      password,
+    };
+    console.log(formData);
+    event.currentTarget.reset();
+  } else {
+    alert("Всі поля мають бути заповнені");
+  }
+}
