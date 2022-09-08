3b84fc45fe10651d33923992ab738c450b49b0a8
TechnicUnion <103375230+TechnicUnion@users.noreply.github.com>
2022-09-08 22:52:24 +0300

    adds task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,17 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+const bodyEL = document.querySelector("body");
+const btnEl = document.querySelector(".change-color");
+const colorNameEl = document.querySelector(".color");
+
+btnEl.addEventListener("click", onBodyColorChange);
+
+function onBodyColorChange() {
+  const randomColor = getRandomHexColor();
+  bodyEL.style.backgroundColor = `${randomColor}`;
+  colorNameEl.textContent = randomColor;
 }
